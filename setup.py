7cca90c29e5b65c469ce43ac49b3bf92498c4e72
Gustavo Picon <tabo@tabo.pe>
2014-09-19 10:50:19 -0500

    Bumping version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class pytest_test(test):
 
 setup_args = dict(
     name='dynpool',
-    version='1.0.3',
+    version='1.0.5',
     url='https://tabo.pe/projects/dynpool/',
     author='Gustavo Picon',
     author_email='tabo@tabo.pe',
