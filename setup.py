f83da79d11b4e235004733ebcefa99d9ba371ec4
Gustavo Picon <tabo@tabo.pe>
2015-01-27 14:23:55 -0500

    Preparing 2.0 release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class pytest_test(test):
 
 setup_args = dict(
     name='dynpool',
-    version='1.0.5',
+    version='2.0',
     url='https://tabo.pe/projects/dynpool/',
     author='Gustavo Picon',
     author_email='tabo@tabo.pe',
